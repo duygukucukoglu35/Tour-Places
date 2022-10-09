6eaa021b93e2c06761daffcbef5c28ddf45709f9
duygukucukoglu35 <duygukucukoglu35@gmail.com>
2022-10-09 09:44:25 +0300

    header component done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.scss';
 import Navbar from './component/Navbar/Navbar';
+import Header from './component/Header/Header';
 
 
 function App() {
   return (
     <div>
     <Navbar/>
+    <Header/>
     <h1>Duygu</h1>
     </div>
   );
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss'; //çektiğimiz dosyalar
+
+
+
+const Header = () => {
+    return(
+        <div className="header">Popular Tour Places</div>
+    )
+}
+
+export default Header // başka komponentte kullanmak için export edilir
