2555585a08b7ddf3985686d5cf7dfc6236a56224
duygukucukoglu35 <duygukucukoglu35@gmail.com>
2022-10-09 09:30:14 +0300

    create navbar import

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.scss';
+import Navbar from './component/Navbar/Navbar';
+
+
+function App() {
+  return (
+    <div>
+    <Navbar/>
+    <h1>Duygu</h1>
+    </div>
+  );
+}
+
+export default App;
